7bf929e6b4161747b96a3512dfc82bf327ea592b
JohnnyMarlison <renat.enokaev@mail.ru>
2020-12-19 21:04:01 +0300

    Fixed and finished task1_14

--- task1_14/main.cpp
+++ task1_14/main.cpp
@@ -2,26 +2,20 @@
 
 int main() {
     // std::cout << "Hello, World!" << std::endl;
-    int x, num, mul = 0, sum = 0, new_num = 0, tmp = 0;
-    float res;
+    int X, Y, mul = 0, sum = 0, res = 0;
     std::cout << "Input X:\n";
-    std::cin >> x;
-    num = x;
-    tmp = num;
+    std::cin >> X;
 
-    for ( mul = 1; x; x /= 10 )
-        mul *= x % 10;
-
-    for ( sum = 0; num; num /= 10 )
-        sum += num % 10;
+    mul = (X / 100) * (X / 10 % 10) * (X / 10);
+    sum = (X / 100) + (X / 10 % 10) * (X / 10);
 
     res = mul % sum;
 
-    new_num = (tmp / 100) * 100 + (tmp % 10) * 10 + (tmp / 10 % 10);
+    Y = (X / 100) * 100 + (X % 10) * 10 + (X / 10 % 10);
     std::cout << "Mul: " << mul << std::endl;
     std::cout << "Sum: " << sum << std::endl;
     std::cout << "Mul % Sum = " << res << std::endl;
-    std::cout << "Y = " << new_num << std::endl;
+    std::cout << "Y = " << Y << std::endl;
 
 
 
