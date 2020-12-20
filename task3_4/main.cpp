b6f4f34ee46b923150eb623ccc3d7abe9db8010a
vadim <akimovvadim779@gmail.com>
2020-12-20 21:09:03 +0300

    Finished task3_4

--- task3_4/main.cpp
+++ task3_4/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, div = 2;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << 1;
+    while (n > 1) {
+        if (n % div == 0) {
+            cout << " * " << div;
+            n = n / div;
+        } else
+            div++;
+    }
     return 0;
 }
