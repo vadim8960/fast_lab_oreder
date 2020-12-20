a5b9a74bf129672f17bb44a0ca5186c19a99ccfd
vadim <akimovvadim779@gmail.com>
2020-12-20 19:45:10 +0300

    Finished task4_4

--- task4_4/main.cpp
+++ task4_4/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double y, y_1 = 0.1, y_2 = -0.1;
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n == 1)
+        cout << y_1 << endl;
+    else if (n == 2)
+        cout << y_2 << endl;
+    else {
+        for (int i = 3; i <= n; ++i) {
+            y = i / 3. * (y_1 * y_1) + (i + 1) / 2. * (y_2 * y_2);
+            y_1 = y_2;
+            y_2 = y;
+        }
+        cout << y << endl;
+    }
     return 0;
 }
