f5ad6af354278887fd765ed601a25d6a31788ba7
vadim <akimovvadim779@gmail.com>
2020-12-20 20:13:10 +0300

    Finished task3_7

--- task3_7/main.cpp
+++ task3_7/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, k;
+    cout << "Enter n, k: ";
+    cin >> n >> k;
+    cout << "0.";
+    int m = 10;
+    for (int i = 0; i < k; ++i) {
+        if (m / n == 0) {
+            cout << '0';
+            m *= 10;
+            continue;
+        }
+        cout << m / n;
+        m = m - (m / n) * n;
+        m *= 10;
+    }
     return 0;
 }
